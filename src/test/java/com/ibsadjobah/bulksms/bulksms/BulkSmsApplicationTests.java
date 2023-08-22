5952fd53a8f4f77a44645db00a091ea3426c7a80
unknown <bahibrahimsadio.com>
2023-08-22 12:28:33 +0000

    first commit

---
+++ src/test/java/com/ibsadjobah/bulksms/bulksms/BulkSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibsadjobah.bulksms.bulksms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkSmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
